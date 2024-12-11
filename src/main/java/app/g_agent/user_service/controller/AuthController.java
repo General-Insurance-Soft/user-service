1ac2a7a115f70f778440000bdf68d8d9cd75997d
Ndiithi <duncanndiithi@yahoo.com>
2024-12-11 09:25:28 +0300

    login endpoint

--- src/main/java/app/g_agent/user_service/controller/AuthController.java
+++ src/main/java/app/g_agent/user_service/controller/AuthController.java
@@ -1,11 +1,17 @@
 package app.g_agent.user_service.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.g_agent.user_service.dto.LoginRequest;
+import app.g_agent.user_service.dto.LoginResponse;
+import app.g_agent.user_service.model.User;
+import app.g_agent.user_service.service.AuthenticationService;
 import app.g_agent.user_service.service.JwtService;
 
 @RestController
@@ -14,35 +20,31 @@ import app.g_agent.user_service.service.JwtService;
 public class AuthController {
 
 //private final JwtService jwtService;
-    
-//    private final AuthenticationService authenticationService;
+
+	@Autowired
+	private AuthenticationService authenticationService;
+	@Autowired
+	private JwtService jwtService;
 
 	@PostMapping("/login")
-	public String login(@RequestBody LoginRequest loginRequest) {
-	  
-	    return "logged in";
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+		User user = authenticationService.authenticate(loginRequest);
+
+		String jwtToken = jwtService.generateToken(user);
+
+		LoginResponse loginResponse = new LoginResponse();
+		loginResponse.setToken(jwtToken);
+		loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+		return ResponseEntity.ok(loginResponse);
 
 	}
 
+	@GetMapping("/index")
+	public String index() {
 
-    public static class LoginRequest {
-        private String username;
-        private String password;
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
+		return "logged in successfully";
 
 	}
 
