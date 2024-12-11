6103122c93a7cc731044c6a1826e94fbbb64f884
Ndiithi <duncanndiithi@yahoo.com>
2024-12-11 09:26:24 +0300

    add dtos

---
+++ src/main/java/app/g_agent/user_service/dto/LoginResponse.java
@@ -0,0 +1,25 @@
+package app.g_agent.user_service.dto;
+
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
+	public long getExpiresIn() {
+		return expiresIn;
+	}
+
+	public void setExpiresIn(long expiresIn) {
+		this.expiresIn = expiresIn;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+ 		
+}
