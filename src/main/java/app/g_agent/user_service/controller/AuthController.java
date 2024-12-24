4dccefbc6e8908c3ffcc56ee5ed48a47e26205ca
Ndiithi <duncanndiithi@yahoo.com>
2024-12-24 10:46:35 +0300

    use exception message

--- src/main/java/app/g_agent/user_service/controller/AuthController.java
+++ src/main/java/app/g_agent/user_service/controller/AuthController.java
@@ -3,6 +3,7 @@ package app.g_agent.user_service.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import app.g_agent.user_service.dto.LoginResponse;
 import app.g_agent.user_service.model.User;
 import app.g_agent.user_service.service.AuthenticationService;
 import app.g_agent.user_service.service.JwtService;
+import app.g_agent.user_service.system.commons.Message;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -27,12 +29,21 @@ public class AuthController {
 	private AuthenticationService authenticationService;
 	@Autowired
 	private JwtService jwtService;
+	@Autowired
+	Message message;
 
 	@PostMapping("/authenticate")
-	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-		
+	public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest) {
+		User user = null;
 		logger.info("Authenticating request =====>");
-		User user = authenticationService.authenticate(loginRequest);
+		try {
+			user = authenticationService.authenticate(loginRequest);
+		} catch (Exception e) {
+			message.setNameString("Unauthorized");
+			message.setMessageString(e.getMessage());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message);
+		}
+
 		logger.info("Attempting to generate JWT =====>");
 		String jwtToken = jwtService.generateToken(user);
 
