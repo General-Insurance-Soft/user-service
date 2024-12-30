e0e95c330def067eb1bdf3266ca1284f9e87d513
Ndiithi <duncanndiithi@yahoo.com>
2024-12-30 18:19:03 +0300

    validate role

--- src/main/java/app/g_agent/user_service/dto/RoleDto.java
+++ src/main/java/app/g_agent/user_service/dto/RoleDto.java
@@ -3,11 +3,12 @@ package app.g_agent.user_service.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class RoleDto {
 
@@ -18,7 +19,8 @@ public class RoleDto {
 
 	@NotNull(message = "Authorities list cannot be null")
 	@NotEmpty(message = "Authorities list cannot be empty")
-	@JsonIgnore
+	@Size(min = 1, message = "Authorities list must have at least one element")
+	@JsonProperty("authorities")
 	private List<Long> authorities;
 
 	@JsonIgnore
