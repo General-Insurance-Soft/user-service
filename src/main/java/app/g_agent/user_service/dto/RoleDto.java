19bf2f870504e7a39b8e343d0dfa97cdea7dc01b
Ndiithi <duncanndiithi@yahoo.com>
2024-12-30 10:25:07 +0300

    ignore fields in reponse

--- src/main/java/app/g_agent/user_service/dto/RoleDto.java
+++ src/main/java/app/g_agent/user_service/dto/RoleDto.java
@@ -2,13 +2,12 @@ package app.g_agent.user_service.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public class RoleDto {
 
@@ -20,8 +19,10 @@ public class RoleDto {
 	@NotNull(message = "Authorities list cannot be null")
 	@NotEmpty(message = "Authorities list cannot be empty")
 	@Valid // Ensure validation is applied to each element in the list
+	@JsonIgnore
 	private List<Long> authorities;
 
+	@JsonIgnore
 	private Long organizationId;
 
 	public Long getRoleId() {
