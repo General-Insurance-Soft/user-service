6e8505dddb8ca964498202a7c37ccb76dd7d14f9
Ndiithi <duncanndiithi@yahoo.com>
2024-12-30 18:22:59 +0300

    implement mapping

--- src/main/java/app/g_agent/user_service/model/Authority.java
+++ src/main/java/app/g_agent/user_service/model/Authority.java
@@ -1,12 +1,18 @@
 package app.g_agent.user_service.model;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Authority {
@@ -17,7 +23,12 @@ public class Authority {
 	@Column(name = "name")
 	private String authorityName;
 
+	@ManyToMany(mappedBy = "authority", fetch = FetchType.EAGER)
+	@JsonBackReference
+	private Set<Role> role = new HashSet<>();
+
 	@Column(name = "created_at")
+
 	private Date createdAt;
 
 	public Long getId() {
@@ -42,6 +53,14 @@ public class Authority {
 
 	public void setAuthorityName(String authorityName) {
 		this.authorityName = authorityName;
+	}
+
+	public Set<Role> getRole() {
+		return role;
+	}
+
+	public void setRole(Set<Role> role) {
+		this.role = role;
 	}
 
 }
